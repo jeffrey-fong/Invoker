850e6d5ef2c2e53f892f250617a09c7d937a5be9
jeffrey-fong <jeffreyfong94@gmail.com>
2023-08-31 23:48:38 +0800

    add model pipeline

---
+++ invoker/model.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+from typing import List, Dict, Any
+
+import torch
+from transformers import StoppingCriteria, StoppingCriteriaList, AutoModelForCausalLM, AutoTokenizer
+
+
+class StopWordsCriteria(StoppingCriteria):
+    def __init__(self, stops=[]):
+        StoppingCriteria.__init__(self)
+        self.stops = stops
+        # Check if stop word starts with "\" and remove first token (empty space)
+        # This is only applicable to vicuna-13b and Nous-Hermes-13b tokenizers
+        for i in range(len(self.stops)):
+            if self.stops[i][0] == 29871:
+                self.stops[i] = self.stops[i][1:]
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
+        inputs = input_ids[0].tolist()
+        for stop in self.stops:
+            if len(inputs) >= len(stop) and inputs[-len(stop) :] == stop:
+                return True
+        return False
+
+
+class InvokerPipeline:
+    # Singleton instance
+    _pipeline = None
+
+    def __init__(self, model_path: str):
+        self._tokenizer = AutoTokenizer.from_pretrained(model_path)
+        # Load model
+        self._model = AutoModelForCausalLM.from_pretrained(
+            model_path, torch_dtype=torch.float16, device_map="auto"
+        )
+
+    async def generate(
+        self, input_text: str, params: List[Dict[str, Any]]
+    ) -> str:
+        prompt = f"""Available Function Headers:
+```python
+# Plan a trip based on user's interests
+def plan_trip(
+	# The destination of the trip
+	destination: str,
+	# The duration of the trip in days
+	duration: int,
+    # The interests based on which the trip will be planned
+    interests: List[str]
+) -> Any:
+```
+
+A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. The assistant calls functions with appropriate input when necessary. 
+
+USER: I want to plan a 7-day trip to Paris with a focus on art and culture
+ASSISTANT:"""
+        # Tokenize the input
+        input_ids = self._tokenizer(prompt, return_tensors="pt").input_ids.cuda()
+        # Run the model to infer an output
+        stop_words_ids = [self._tokenizer.encode(stop_word, add_special_tokens=False) for stop_word in ["\nUSER:", "\nASSISTANT:", "\nFUNCTION:"]]
+        stopping_criteria = StoppingCriteriaList([StopWordsCriteria(stops=stop_words_ids)])
+        output_ids = self._model.generate(input_ids=input_ids, max_new_tokens=512, do_sample=True, top_p=0.9,temperature=0.001, stopping_criteria=stopping_criteria)
+        output = self._tokenizer.decode(output_ids[0], skip_special_tokens=True)
+        return output
+
+    @classmethod
+    async def maybe_init(cls, model_path: str) -> InvokerPipeline:
+        if cls._pipeline is None:
+            cls._pipeline = InvokerPipeline(model_path=model_path)
+        if cls._pipeline is not None:
+            return cls._pipeline
+        else:
+            raise ValueError("Pipeline could not be initialized!")
--- serve_fastapi.py
+++ serve_fastapi.py
@@ -1,16 +1,35 @@
 from typing import List, Dict
+from typing_extensions import Annotated
 import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import uvicorn
+import typer
 
 from invoker.api_types import ChatInput, ChatOutput
+from invoker.model import InvokerPipeline
 
 
 app = FastAPI(title="Invoker")
 
 
+async def get_pipeline(model_path: str):
+    return await InvokerPipeline.maybe_init(model_path=model_path)
+
+
 @app.post('/chat', response_model=ChatOutput)
-def chat(req: ChatInput):
+def chat(req: ChatInput, invoker_pipeline: InvokerPipeline = Depends(get_pipeline)):
     id = str(uuid.uuid4())
     message = {"role": "assistant", "content": "Endpoint called"}
     return {"id": id, "choices": [{"message": message, "finish_reason": "stop"}]}
+
+
+def main(
+    model_path: Annotated[str, typer.Argument(help="The path to the model to use")] = "jeffrey-fong/invoker-13b"
+):
+    invoker_pipeline = get_pipeline(model_path=model_path)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    typer.run(main)
